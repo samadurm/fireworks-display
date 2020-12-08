11999a8006b0f4e98945af6d09f691ebdfb9dc6a
samadurm <samadurm@oregonstate.edu>
2020-12-08 07:59:58 -0800

    Added timing in Animate() and added more functions for rocket movement

--- include/rocket.hpp
+++ include/rocket.hpp
@@ -9,28 +9,72 @@
 
 class Rocket {
     public:
-        Rocket(float velocity, float x, float y, float z, GLuint cone, GLuint stem)
-            : velocity(velocity), x(x), y(y), z(z), cone(cone), stem(stem) {
+        Rocket(float velocity, float x, float y, float z)
+            : velocity(0.1), x(x), y(y), z(z) {
                 height = 0.2;
+                isExploded = false;
             }
         ~Rocket() {}
 
+        void bindObjects(GLuint cone, GLuint stem) {
+            this->cone = cone;
+            this->stem = stem;
+        }
+
+        // void move(float amount) {
+        //     this->y += amount;
+        // }
+
+        void accelerate(const float g) {
+            if (velocity > 0) {
+                velocity -= g;
+                y += velocity;
+                printf("Velocity is now %f and y is %f\n", velocity, y);
+            } else if (velocity < 0) {
+                velocity = 0;
+                isExploded = true;
+            }
+        }
+
+        void drawFireworks() {
+            if (isExploded) {
+                drawExplosion();
+            } else {
+                drawRocket();
+            }
+        }
+
+        void setColor(const GLfloat *color) {
+            this->color = *color;
+        }
+
+    private:
         void drawRocket() {
+            
+            // this is the rocket portion
             glPushMatrix();
+                glColor3fv(&color);
                 glTranslatef(x, y, z);
                 glRotatef(-90., 1., 0., 0.);
                 glCallList(cone);
             glPopMatrix();
 
+            // this is the stem portion
             glPushMatrix();
                 glColor3f(1., 1., 1.);
                 glTranslatef(x, y, z);
                 glCallList(stem);
             glPopMatrix();
         }
+
+        void drawExplosion() {
+            printf("Rocket has exploded, must draw fireworks");
+        }
         
-    private:
+
         float velocity, x, y, z;
         float height;
         GLuint cone, stem;
+        GLfloat color;
+        bool isExploded;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -181,6 +181,9 @@ int		WhichColor;				// index into Colors[ ]
 int		WhichProjection;		// ORTHO or PERSP
 int		Xmouse, Ymouse;			// mouse values
 float	Xrot, Yrot;				// rotation angles in degrees
+Rocket  *rocket1;
+float   Elapsed, Time;
+#define MS_PER_CYCLE	1000
 
 
 // function prototypes:
@@ -276,6 +279,11 @@ Animate( )
     // Idea: put an if statement where we start the normal 1 second (or half second) animation
     // cycle for the explosion
 
+    int ms = glutGet( GLUT_ELAPSED_TIME );
+    Elapsed = ms;
+    ms %= MS_PER_CYCLE;
+    Time = (float)ms / (float)MS_PER_CYCLE;		// [0.,1.)
+
     // if (Exploded) {
 
     // } else {
@@ -351,7 +359,7 @@ Display( )
 
 	// set the eye position, look-at position, and up-vector:
 
-	gluLookAt( 0., 0., 3.,     0., 0., 0.,     0., 1., 0. );
+	gluLookAt( 0., 0., 10.,     0., 0., 0.,     0., 1., 0. );
 
 
 	// rotate the scene:
@@ -397,7 +405,6 @@ Display( )
 
 	glEnable( GL_NORMALIZE );
 
-
 	// draw the current object:
     glPushMatrix();
     glColor3f(0.5, 0.5, 0.5);
@@ -412,9 +419,13 @@ Display( )
     // glRotatef(-90., 1., 0., 0.);
     // glCallList(ConeList);
     // glPopMatrix();
-    glColor3f(1., 0.0, 0.0);
-    Rocket rocket(3, 0., 1.0, 0.3, ConeList, StemList);
-    rocket.drawRocket();
+    
+    rocket1->setColor(&Colors[WhichColor][0]);
+    rocket1->bindObjects(ConeList, StemList);
+
+    // rocket1->accelerate(-0.098); // -g
+    // rocket1->move(0.0001*Elapsed);
+    rocket1->drawFireworks();
 
 
 	if( DepthFightingOn != 0 )
@@ -731,7 +742,7 @@ InitGraphics( )
 	glutTabletButtonFunc( NULL );
 	glutMenuStateFunc( NULL );
 	glutTimerFunc( -1, NULL, 0 );
-	glutIdleFunc( NULL );
+	glutIdleFunc( Animate );
 
 	// init glew (a window must be open to do this):
 
@@ -747,6 +758,8 @@ InitGraphics( )
 #endif
 
     quad = gluNewQuadric(); // this is for the stem
+    glColor3f(1., 0.0, 0.0);
+    rocket1 = new Rocket(0.1, 0., 1.0, 0.3);
 }
 
 
